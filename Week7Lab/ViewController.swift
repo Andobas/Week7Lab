7e9b67fcccf8cd9737ffd82aed8613d10ed40232
Andobas <one.e.toe@gmail.com>
2016-03-10 21:05:43 -0800

    last commit

--- Week7Lab/ViewController.swift
+++ Week7Lab/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(sender: UIPanGestureRecognizer) {
+        // Absolute (x,y) coordinates in parent view's coordinate system
+        let point = UIPanGestureRecognizer.locationInView(parentView)
+        
+        // Total translation (x,y) over time in parent view's coordinate system
+        let translation = panGestureRecognizer.translationInView(parentView)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+        
+        
+    }
 
 }
 
